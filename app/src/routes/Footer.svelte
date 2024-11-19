e4003528c882415e177ddbd25334d66eb23d3861
Diego Fernández <diego.j.fernandez.g@gmail.com>
2024-11-19 16:22:43 -0300

    Armado de la estructura base del front (con posibles cambios a futuro...)

---
+++ app/src/routes/Footer.svelte
@@ -0,0 +1,5 @@
+<h5>Copyright © 2024 Equipo Rockect
+All Rights Reserved
+ 
+This product is protected by copyright and distributed under
+licenses restricting copying, distribution, and decompilation.</h5>
---
+++ app/src/routes/Header.svelte
@@ -0,0 +1,23 @@
+<header>
+    <nav>
+        <a class=nav-button href="/">Menú de inicio</a>
+        <a class=nav-button href="/pokemons">Pokemons</a>
+        <a class=nav-button href="/movimientos">Movimientos</a>
+        <a class=nav-button href="/equipos">Equipos</a>
+    </nav>
+</header>
+
+<style>
+	nav {
+		display: flex;
+		justify-content: space-around;
+	}
+    .nav-button {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 200px;
+        height: 50px;
+        background-color: aqua;
+    }
+</style>
